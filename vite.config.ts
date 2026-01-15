428fbde1b2d4dcb4d9247cf0294b033cb543f5e6
Oswaldo Vazquez <ovazquez@Arcoss-MacBook-Air.local>
2026-01-15 15:39:47 -0600

    Update Vite config for Builder.io compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,11 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    open: true,
+    open: false,
+    middlewareMode: true,
+  },
+  build: {
+    target: 'esnext',
+    minify: false,
   },
 });
